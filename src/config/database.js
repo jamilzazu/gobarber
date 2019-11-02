0011d906c1864eff3deba986c271cafa7699ef67
Jamil Zazu <jamilzazu@gmail..com>
2019-11-02 00:28:57 -0400

    Configurando o sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postegres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
