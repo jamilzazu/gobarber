cde633c4d07ff7c3c9cff2e5402c32ef2d8dad04
Jamil Zazu <jamilzazu@gmail..com>
2019-11-01 21:19:24 -0400

    Configurando o nodemon e sucrase

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const routes = require('./routes');
+import express from 'express';
+import routes  from './routes';
 
 class App{
   constructor(){
@@ -18,4 +18,4 @@ this.server.use(express.json());
   }
 }
 
-module.exports = new App().server;
+export default new App().server;
