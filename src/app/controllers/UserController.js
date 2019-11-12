86b59e35ecaaaa5b74669438af728fe4de47afcf
Jamil Zazu <jamilzazu@gmail..com>
2019-11-12 12:29:56 -0400

    Update do usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,8 +16,27 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: true });
+    const { email, oldpassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldpassword && !(await user.checkPassword(oldpassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
